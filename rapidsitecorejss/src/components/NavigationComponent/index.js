e9b1749b9dd5fd8fecc4f72c4e46306b851633b5
Radygin, Kirill  (Arcadia) <kirill.radygin@arcadia.spb.ru>
2019-05-15 20:36:52 +0300

    Navigation component

--- rapidsitecorejss/src/components/NavigationComponent/index.js
+++ rapidsitecorejss/src/components/NavigationComponent/index.js
@@ -1,29 +1,57 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavigationComponent = (props) => (
-  <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom">
-    <h5 className="my-0 mr-md-auto font-weight-normal">
-      <NavLink to="/" className="text-dark">
+// const dropdownLink = (id) => 
+//   <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+//       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//       Dropdown
+//   </a>;
 
-      </NavLink>
-    </h5>
-    <nav className="my-2 my-md-0 mr-md-3">
-      <a
-        className="p-2 text-dark"
-        href="https://jss.sitecore.net"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-      </a>
-      <NavLink to="/styleguide" className="p-2 text-dark">
-        Styleguide
-      </NavLink>
-      <NavLink to="/graphql" className="p-2 text-dark">
-        Documentation
-      </NavLink>
-    </nav>
-  </div>
-);
+const NavigationComponent = (props) => {
+  console.log(props);
+  return (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href={props.href}>
+      <img src="https://picsum.photos/80/50" className="logo" alt="logo" />
+    </a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          {
+            !props.fields.links ? null : props.fields.links.value.map((link, i) => {
+              const isActive = link.url === window.location.pathname;
+              // const id = link.url + i;
+
+              return (
+                <li className={'nav-item' + isActive ? ' active' : '' + link.list ? ' dropdown' : ''}
+                  // {
+                  //   link.list ? ` id="${id}" role="button"
+                  //   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"`
+                  //   : ''
+                  // }  
+                >
+                    <NavLink to={link.url} className={'nav-link' + link.isDisabled ? ' disabled' : ''}>
+                      {link.text}
+                      {isActive ? <span className="sr-only">(current)</span> : null}
+                    </NavLink>
+                    {/* {
+                      link.list ? 
+                        <div className="dropdown-menu float-right" aria-labelledby="{}">
+                          {
+                            link.list.map(subLink =>
+                              <a className="dropdown-item" href={subLink.url}>{subLink.text}</a>
+                            )
+                          }
+                        </div>
+                        : null
+                    } */}
+                </li>
+              );
+            })
+          }
+        </ul>
+    </div>
+  </nav>
+)
+};
 
 export default NavigationComponent;
