1883a09b551c34f1e001ba743c26897d0f9f35b7
rafael <rfernandez@thedeal.com>
2019-05-15 12:04:22 +0100

    React hooks and RTE

--- rapidsitecorejss/src/components/RichTextEditor/index.js
+++ rapidsitecorejss/src/components/RichTextEditor/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text } from '@sitecore-jss/sitecore-jss-react';
+import ReactQuill from 'react-quill'; 
+import 'react-quill/dist/quill.snow.css';
 
-const RichTextEditor = (props) => (
-  <div>
-    <p>RichTextEditor Component</p>
-    <Text field={props.fields.heading} />
-    <Text field={props.fields.body} />
-  </div>
-);
+const RichTextEditor = (props) => {
+  const [text, textChanged] = useState('');
+
+  //Embedded style only until we have a base theme
+  return (
+    <div style={{margin:'15px',padding:'15px',border:'1px solid #CCC'}}>
+      <p>RichTextEditor Component</p>
+      <Text field={props.fields.heading} />
+      <Text field={props.fields.body} />
+      <ReactQuill 
+        style={{margin:'15px'}} 
+        value={text} 
+        onChange={textChanged} />
+    </div>
+  );
+}
 
 export default RichTextEditor;
