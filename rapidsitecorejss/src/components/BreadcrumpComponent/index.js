a7f553a53ebc857e3a9a91fc79c052926a2b283e
GOWI\dimitrijez <dimitrije.zoroski@gowi.rs>
2019-05-16 14:06:39 +0200

    cta and brandcrump

--- rapidsitecorejss/src/components/BreadcrumpComponent/index.js
+++ rapidsitecorejss/src/components/BreadcrumpComponent/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { Text } from '@sitecore-jss/sitecore-jss-react';
 
 const BreadcrumpComponent = (props) => (
-  <div>
-    <p>BreadcrumpComponent Component</p>
-    <Text field={props.fields.heading} />
-  </div>
+  <nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Library</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Data</li>
+  </ol>
+  </nav>
+  
 );
 
 export default BreadcrumpComponent;
