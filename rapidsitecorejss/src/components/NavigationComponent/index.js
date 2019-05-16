45e0a4631e823f861198630328aaff5ba884c81c
GOWI\dimitrijez <dimitrije.zoroski@gowi.rs>
2019-05-16 12:10:53 +0200

    fixed yml

--- rapidsitecorejss/src/components/NavigationComponent/index.js
+++ rapidsitecorejss/src/components/NavigationComponent/index.js
@@ -16,17 +16,19 @@ const links = [
   {
     text: 'Search',
     url: '/search'
+  },
+  {
+    text: 'Home',
+    url: '/search'
   }
 ];
 
 const NavigationComponent = (props) => {
   return (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href={rootUrl}>
-      <img src={logoUrl} className="logo" alt="logo" />
-    </a>
+   
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
+        <ul className="navbar-nav mr-auto float-right">
           {
             !links ? null : links.map(link => {
               const isActive = link.url === window.location.pathname;
