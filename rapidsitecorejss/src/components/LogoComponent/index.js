7a413460919e7977817f943a8f166998bb06df85
Radygin, Kirill  (Arcadia) <kirill.radygin@arcadia.spb.ru>
2019-05-16 12:21:42 +0300

    Updated

--- rapidsitecorejss/src/components/LogoComponent/index.js
+++ rapidsitecorejss/src/components/LogoComponent/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Image } from '@sitecore-jss/sitecore-jss-react';
 
 const LogoComponent = (props) => (
-  <a class="navbar-brand" href="#">  <Image field={props.fields.imageFields} class="logo img img-responsive" /> </a>
+  <a class="navbar-brand" href="/">
+    <Image field={props.fields.imageFields} class="logo img img-responsive" />
+  </a>
 );
 
 export default LogoComponent;
--- rapidsitecorejss/src/components/NavigationComponent/index.js
+++ rapidsitecorejss/src/components/NavigationComponent/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Text, Image } from '@sitecore-jss/sitecore-jss-react';
 
-<<<<<<< HEAD
 // const dropdownLink = (id) => 
 //   <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
 //       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -66,42 +65,5 @@ const NavigationComponent = (props) => {
   </nav>
 )
 };
-=======
-const NavigationComponent = (props) => (
-  <>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav w-100 justify-content-end">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div class="dropdown-menu float-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                </ul>
-            </div>
-       
-            </>
-);
->>>>>>> origin/master
 
 export default NavigationComponent;
