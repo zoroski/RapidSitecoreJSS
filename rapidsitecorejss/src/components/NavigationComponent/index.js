ccdf8090a09438aeee734c66a1bbbb688309a81b
Natalia Alexeenko <natalia.alexeenko@arcadia.spb.ru>
2019-05-16 11:54:49 +0100

    Fixed header alignment behaviour.

--- rapidsitecorejss/src/components/NavigationComponent/index.js
+++ rapidsitecorejss/src/components/NavigationComponent/index.js
@@ -25,10 +25,10 @@ const links = [
 
 const NavigationComponent = (props) => {
   return (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <nav className="navbar navbar-expand-lg navbar-light bg-light w-100">
    
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto float-right">
+        <ul className="navbar-nav w-100 justify-content-end">
           {
             !links ? null : links.map(link => {
               const isActive = link.url === window.location.pathname;
